a3987a1991c03a750785efcf01111915adf51ea0
david-basil <72323323+david-basil@users.noreply.github.com>
2021-10-14 17:10:44 -0400

    not sure what's up here's a bit more

--- src/main/java/user.java
+++ src/main/java/user.java
@@ -5,12 +5,15 @@ public class User {
     private int studentNumber;
     private String name;
     private String location;
-    private Map reviews;
-
+    private HashMap reviews;
+    private HashMap filters;
 
     public User(String name, int studentNumber) {
         this.name = name;
-        this.studentNumber = studentNumber
+        this.studentNumber = studentNumber;
+        this.location = "";
+        this.reviews = new HashMap<Building, Review>();
+        this.filters = new HashMap<String, boolean>;
     }
 
 }
