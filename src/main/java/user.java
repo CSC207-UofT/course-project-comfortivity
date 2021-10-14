baa505541087b3aa06373c44901b6af20e975466
david-basil <72323323+david-basil@users.noreply.github.com>
2021-10-14 14:29:13 -0400

    hey! i started, most of these files are empty, just threw some code into building. will do more later!

---
+++ src/main/java/user.java
@@ -0,0 +1,16 @@
+import java.util.Map;
+import java.util.List;
+
+public class User {
+    private int studentNumber;
+    private String name;
+    private String location;
+    private Map reviews;
+
+
+    public User(String name, int studentNumber) {
+        this.name = name;
+        this.studentNumber = studentNumber
+    }
+
+}
