b4515bd6ea2f376e3af636acc1b7264b0bfe2fe9
david-basil <72323323+david-basil@users.noreply.github.com>
2021-10-14 21:32:42 -0400

    not sure what's up here's a bit more

--- src/main/java/CLI.java
+++ src/main/java/CLI.java
@@ -1,3 +1,16 @@
-public static void main(String[]args){
+public class CLI {
+    public static void main(String[] args) {
+        system.out.println("please enter your name")
+        {
+            // Enter data using BufferReader
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
 
+            // Reading data using readLine
+            String name = reader.readLine();
+
+            // Printing the read line
+            System.out.println(name);
         }
+    }
+}
