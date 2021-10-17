5eb307da2ca76f3f171d3668ad89065d968c532f
Gurman <90292710+BetaGemPsi@users.noreply.github.com>
2021-10-16 21:17:21 -0400

    Update NewReviewRequest.java

--- src/main/java/NewReviewRequest.java
+++ src/main/java/NewReviewRequest.java
@@ -1,6 +1,6 @@
 public class NewReviewRequest implements RequestInterface{
     int student_id;
-    int review;
+    Review review;
 
     public NewReviewRequest(int student_id, Review review) {
         this.student_id = 0;
