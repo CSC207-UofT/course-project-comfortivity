b1567c483e0bb4fe1dc0a97944a3e8b36732751b
Gurman <90292710+BetaGemPsi@users.noreply.github.com>
2021-10-16 21:16:39 -0400

    Update Review.java

--- src/main/java/Review.java
+++ src/main/java/Review.java
@@ -1,8 +1,16 @@
 
 public class Review {
     int rating;
+    String comment;
     
     public Review(int rate) {
         this.rating = rate;
+        this.comment = "";   
+    }
+    
+    public Review(int rate, String comment) {
+        this.rating = rate;
+        this.comment = comment;
+        
     }
 }
