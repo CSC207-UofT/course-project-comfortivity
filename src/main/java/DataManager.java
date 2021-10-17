951b41f9628001a40f0785670a4ac8e2b651fa34
Gurman <beta.psi@outlook.com>
2021-10-16 20:07:20 -0400

    Sample DataManager and UIcontroller.

---
+++ src/main/java/DataManager.java
@@ -0,0 +1,30 @@
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class DataManager {
+
+    public void updateNewReview() {
+        //
+    }
+
+    public void updateNewUser() {
+        //
+    }
+
+    public Collection<Object> profileReturn(int studentNumber) {
+        // Load the data from the given file corresponding to the student with number studentNumber
+        // into a Collection object info and return it.
+        // THIS IS JUST TEMPORARY;
+        List<Object> studentdata = Arrays.aslist("", 0);
+        return studentdata;
+    }
+
+    public Collection<Building> buildingReturn() {
+        // Load all the data for the buildings from the given file as Building objects in a Collection object
+        // THIS IS JUST TEMPORARY;
+        List<Building> buildings = new List[0];
+        return buildings;
+    }
+}
