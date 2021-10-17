c4bec126c5b9c997a70bb2ca8803cc99cb0977f0
david-basil <72323323+david-basil@users.noreply.github.com>
2021-10-16 21:32:05 -0400

    casting stuff

--- src/main/java/UIController.java
+++ src/main/java/UIController.java
@@ -2,19 +2,20 @@ import java.util.Map;
 import java.util.List;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Collection;
 
 
 public class UIController {
 
     public void processRequest(NewReviewRequest req) {
-        return DataManager.updateNewReview(req.student_id, req.review, req.revbuilding);
+        DataManager.updateNewReview(req.student_id, req.review, req.revbuilding);
     }
 
     public void processRequest(NewUserRequest req) {
-        return DataManager.updateNewUser(req.student_id, req.name);
+        DataManager.updateNewUser(req.student_id, req.name);
     }
 
     public Collection<Object> processRequest(RetrieveProfileRequest req) {
-        return DataManager.profileReturn(req.student_number);
+        return DataManager.profileReturn(req.student_id);
     }
 }
