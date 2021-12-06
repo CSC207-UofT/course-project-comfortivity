d20effc56338c710d0cc306fd49845a1392fb828
Edmund Rong <e.rong@mail.utoronto.ca>
2021-12-06 16:01:32 -0500

    rewrote Building

--- src/main/java/Entities/Building.java
+++ src/main/java/Entities/Building.java
@@ -5,73 +5,109 @@ import UIDataManaging.DataManager;
 import java.util.*;
 
 
-public class Building implements Buildable {
+public class Building {
+    private String code;
     private String name;
     private String address;
     private int star_rating;
+    private boolean group;
+    private boolean individual;
     private boolean bathrooms;
-    private ArrayList<Review> reviews;
+    private boolean water;
+    private boolean food;
+    private boolean accessibility;
 
-
-
-    public static Collection<Building> load_buildings() {
-
-        Collection<Building> bldgs_to_return = DataManager.buildingReturn();
-        return bldgs_to_return;
-        // Eventually it'll involve something like this, for now I'll just be like
-
-
-    }
-
-
-    public Building(){
+    public Building() {
+        this.code = "";
         this.name = "";
         this.address = "";
         this.star_rating = 0;
+        this.group = false;
+        this.individual = false;
         this.bathrooms = false;
-        this.reviews = new ArrayList<Review>();
-
+        this.water = false;
+        this.food = false;
+        this.accessibility = false;
     }
 
-    public Building(String name, String address){
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
-        this.address = address;
-        this.star_rating = 0;
-        this.bathrooms = false;
-
-    }
-    public Building(String name){
-        this.name = name;
-        this.address = "";
-        this.star_rating = 0;
-        this.bathrooms = false;
-
-    }
-    public Building(String name, String address, boolean bathrooms, int starRating){
-        this.name = name;
-        this.address = address;
-        this.bathrooms = bathrooms;
-        this.star_rating = starRating;
-
-    }
-
-    public int getStarRating() {
-        return this.star_rating;
     }
 
     public String getAddress() {
-        return this.address;
+        return address;
     }
 
-    public String getName() {return this.name;}
-    public HashMap<String, Object> getAttributes() {
-        HashMap<String, Object> attributes = new HashMap<String, Object>();
-        attributes.put("address", this.address);
-        attributes.put("starRating", this.star_rating);
-        attributes.put("bathrooms", this.bathrooms);
-        return attributes;
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
+    public int getStar_rating() {
+        return star_rating;
+    }
 
+    public void setStar_rating(int star_rating) {
+        this.star_rating = star_rating;
+    }
+
+    public boolean isGroup() {
+        return group;
+    }
+
+    public void setGroup(boolean group) {
+        this.group = group;
+    }
+
+    public boolean isIndividual() {
+        return individual;
+    }
+
+    public void setIndividual(boolean individual) {
+        this.individual = individual;
+    }
+
+    public boolean isBathrooms() {
+        return bathrooms;
+    }
+
+    public void setBathrooms(boolean bathrooms) {
+        this.bathrooms = bathrooms;
+    }
+
+    public boolean isWater() {
+        return water;
+    }
+
+    public void setWater(boolean water) {
+        this.water = water;
+    }
+
+    public boolean isFood() {
+        return food;
+    }
+
+    public void setFood(boolean food) {
+        this.food = food;
+    }
+
+    public boolean isAccessibility() {
+        return accessibility;
+    }
+
+    public void setAccessibility(boolean accessibility) {
+        this.accessibility = accessibility;
     }
 
     public void addReview(Review review){
