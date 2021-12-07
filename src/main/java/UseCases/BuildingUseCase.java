a1f77e6dd7e9f2b138ed062e544814b1e9d3ecb0
Edmund Rong <e.rong@mail.utoronto.ca>
2021-12-06 19:13:05 -0500

    refactored schoolmap as a gateway

--- src/main/java/UseCases/BuildingUseCase.java
+++ src/main/java/UseCases/BuildingUseCase.java
@@ -12,22 +12,8 @@ public class BuildingUseCase {
     BuildingDataInterface buildingDataInterface;
 
     public static Building loadBuilding(String code, BuildingDataInterface bdi) {
-//        building.setCode((String) info.get(0));
-//        building.setName((String) info.get(1));
-//        building.setAddress((String) info.get(2));
-//        building.setStar_rating((Integer) info.get(3));
-//        building.setGroup((Boolean) info.get(4));
-//        building.setIndividual((Boolean) info.get(5));
-//        building.setBathrooms((Boolean) info.get(6));
-//        building.setWater((Boolean) info.get(7));
-//        building.setFood((Boolean) info.get(8));
-//        building.setAccessibility((Boolean) info.get(9));
-//        bdi.retrieveBuildingInfo(code);
+
         return bdi.retrieveBuildingInfo(code);
-    }
-
-    public static void loadAllBuildings() {
-
     }
 
     public static Building getBuildingToReview(schoolMap campusMap) throws IOException, InterruptedException
