8f4c7b061389f33fd8c6bc556f4d964e9c135878
gwodu <gesi.wodu@mail.utoronto.ca>
2021-12-08 22:49:16 -0500

    wrote unit test for userbuilder and userbuilderdirector and building classes Deleted userbuilderdirectorunittests.java

--- src/test/java/userunittests.java
+++ src/test/java/userunittests.java
@@ -1,31 +1,51 @@
+import Entities.Review;
 import Entities.User;
+import Entities.UserBuilder;
+import Entities.UserBuilderDirector;
+import Entities.Building;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 class UserTest {
-    User user;
+    UserBuilder userBuilder;
+    UserBuilderDirector userBuilderDirector;
 
     public static void main(String[] args) {
         UserTest hey = new UserTest();
         hey.setUp();
         hey.testCreateUser();
     }
+
     @Before
     public void setUp() {
-        user = new User();
+        userBuilder = new UserBuilder();
+        userBuilderDirector = new UserBuilderDirector(userBuilder);
+
     }
 
     @Test
     public void testCreateUser() {
-        User newUSER = new User("Mary", "1002345678");
-        assertEquals(attributes.get("name"), "Mary");
-        System.out.println("gotit");
+
+        User user = userBuilderDirector.startUserBuild(1005637, "Nnamdi Azikiwe");
+        assertEquals(user.getStudentNumber(), 1005637);
+        assertEquals(user.getName(), "Nnamdi Azikiwe");
+        System.out.println("Works");
     }
-    public void testCreateUser() {
-        User newUSER = new User("Peter", "1009876543");
-        assertEquals(attributes.get("studentNumber"), "1009876543");
-        System.out.println("gotit");
+
+    public void testCreateUserLarge() {
+        HashMap<Building, String> reviews = new HashMap<>();
+        HashMap<String, Boolean> preferences = new HashMap<>();
+        User user = userBuilderDirector.startUserBuild(1005637, "Nnamdi Azikiwe", "81 St. Maru Stree", reviews, preferences);
+        assertEquals(user.getStudentNumber(), 1005637);
+        assertEquals(user.getName(), "Nnamdi Azikiwe");
+        assertEquals(user.getlocation(), "81 St. Mary Street");
+        assertEquals(user.getReviews(), reviews);
+        assertEquals(user.getPreferences(), preferences);
+    }
 }
