2a0b04717624757b49f17f17c15e47ddc889821e
Edmund Rong <e.rong@mail.utoronto.ca>
2021-12-08 23:13:41 -0500

    rewrote buildinggateway

--- src/main/java/UIDataManaging/MongoDB.java
+++ src/main/java/UIDataManaging/MongoDB.java
@@ -25,38 +25,38 @@ public class MongoDB {
     public static void main(String[] args) {
         schoolMap new_map = new schoolMap();
         String uri = "mongodb+srv://Comfortivity:CSC207@cluster0.rgnj6.mongodb.net/comfortivity?retryWrites=true&w=majority";
-        for (char alphabet = 'A'; alphabet <= 'F'; alphabet++) {
-            for (int i = 1; i <= 5; i++) {
-                String address = alphabet + String.valueOf(i);
-                ArrayList<Building> buildings = new ArrayList<>();
-                BuildingDataInterface bdi = new BuildingGateway();
-                try (MongoClient mongoClient = MongoClients.create(uri)) {
-                    MongoDatabase database = mongoClient.getDatabase("comfortivity");
-                    MongoCollection<Document> collection = database.getCollection("buildings");
-                    try {
-                        DistinctIterable<String> docs = collection.distinct(
-                                "code", Filters.eq("address", address),
-                                String.class);
-                        MongoCursor<String> results = docs.iterator();
-                        while(results.hasNext()) {
-                            buildings.add(BuildingUseCase.loadBuilding(results.next(), bdi));
-                        }
-                        // new_map.put(address, buildings);
-                    } catch (MongoException me) {
-                        System.err.println("An error occurred: " + me);
-                }
+//        for (char alphabet = 'A'; alphabet <= 'F'; alphabet++) {
+//            for (int i = 1; i <= 5; i++) {
+//                String address = alphabet + String.valueOf(i);
+//                ArrayList<Building> buildings = new ArrayList<>();
+//                BuildingDataInterface bdi = new BuildingGateway();
+//                try (MongoClient mongoClient = MongoClients.create(uri)) {
+//                    MongoDatabase database = mongoClient.getDatabase("comfortivity");
+//                    MongoCollection<Document> collection = database.getCollection("buildings");
+//                    try {
+//                        DistinctIterable<String> docs = collection.distinct(
+//                                "code", Filters.eq("address", address),
+//                                String.class);
+//                        MongoCursor<String> results = docs.iterator();
+//                        while(results.hasNext()) {
+//                            buildings.add(BuildingUseCase.loadBuilding(results.next(), bdi));
+//                        }
+//                        // new_map.put(address, buildings);
+//                    } catch (MongoException me) {
+//                        System.err.println("An error occurred: " + me);
+//                }
+//            }
+//        }
+//    }
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("comfortivity");
+            MongoCollection<Document> collection = database.getCollection("buildings");
+            try {
+                String doc = collection.distinct("name", eq("code", "RW"), String.class).first();
+                System.out.println(doc);
+            } catch (MongoException me) {
+                System.err.println("An error occurred: " + me);
             }
         }
-    }
-
-//    public static void newUser(MongoClient mongoClient, User user) {
-//        try {
-//            MongoDatabase database = mongoClient.getDatabase("comfortivity");
-//            MongoCollection<Document> collection = database.getCollection("users");
-//            InsertOneResult result = collection.insertOne(new Document().append("name", user.getName()).append("id", user.getStudentNumber()));
-//            System.out.println("Success! Inserted document id: " + result.getInsertedId());
-//        } catch (MongoException me) {
-//            System.err.println("Unable to insert due to an error: " + me);
-//        }
     }
 } 
